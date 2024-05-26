74d40d59345c90f6ce81b920332240b3f41b430f
Long <longnp@btc-studio.io>
2024-05-27 01:23:59 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,198 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
+import { Button, Carousel, Form, Input, message } from 'antd';
+import Markdown from 'markdown-to-jsx';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
+  const benefits = [
+    {
+      title: 'Quản lý số lượng sản phẩm',
+      description: 'Theo dõi số lượng sản phẩm nhập về một cách trực quan',
+    },
+    ,
+    {
+      title: 'Tạo hoá đơn dễ dàng',
+      description: 'Tạo hoá đơn thanh toán công nợ chỉ bằng 1 thao tác',
+    },
+    {
+      title: 'Theo dõi công nợ & thanh toán',
+      description:
+        'Theo dõi công nợ tránh bỏ lỡ kỳ thanh toán - tạo niềm tin với nhà cung cấp',
+    },
+  ];
+
+  const guideContent = `
+  Cảm ơn bạn đã tin tưởng Invoy, một ứng dụng giúp bạn tối ưu hoá quy trình nhập hàng và thanh toán công nợ! Làm theo hướng dẫn sau để bắt đầu sử dụng ứng dụng.
+  <br><br/>
+  1. **Tải ứng dụng từ App Store:**
+      Mở App Store, tìm kiếm và tải ứng dụng Invoy.
+
+  2. **Tạo Nhà cung cấp:**
+      Mở ứng dụng và tạo các nhà cung cấp là đối tác của bạn.
+
+  3. **Thêm sản phẩm cho các Nhà cung cấp:**
+      Sản phẩm bao gồm tên, giá mua, ảnh. App cung cấp tính năng tạo sản phẩm liên tiếp khi bạn cần nhập số lượng lớn.
+
+  4. **Thêm thông tin nhận hàng:**
+      Khi bạn nhận được hàng từ Nhà cung cấp, hãy tạo một đơn nhập hàng bằng cách: Xem chi tiết một sản phẩm > Bấm nút Thêm ở mục Lịch sử trả hàng.
+
+  5. **Tạo hoá đơn:**
+      Mở màn hình Nhà cung cấp, bấm nút Xuất h.đơn. Tại màn hình Tạo hoá đơn, bấm nút Thanh toán. Bạn có thể chụp màn hình rồi chia sẻ hoá đơn đến Nhà cung cấp.
+  <br><br/>
+  Chúng tôi hi vọng bạn có trải nghiệm tuyệt vời với Invoy! Nếu bạn có bất kỳ thắc mắc nào, hãy liên hệ với chúng tôi ở mục "Liên hệ" ở website.
+  `;
+
+  const [form] = Form.useForm();
+
+  const handleSubmit = async (values: any) => {
+    try {
+      // Implement logic to send contact form data (e.g., email API)
+      console.log('Contact form submitted:', values);
+      message.success('Tin nhắn của bạn được gửi thành công!');
+      form.resetFields();
+    } catch (error) {
+      console.error('Error submitting contact form:', error);
+      message.error('Xảy ra lỗi. Hãy thử lại.');
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='bg-gray-100'>
+      <section className='relative hero min-h-screen flex flex-col justify-center items-center'>
+        <div className='absolute w-full h-screen z-0 opacity-35'>
+          <Image src='/icon.png' alt='Invoy App' fill />
         </div>
-      </div>
+        <div className='w-screen h-screen z-10 backdrop-blur-3xl'>
+          <div className='w-[720px] mx-auto mt-32'>
+            <div className='flex'>
+              <Image
+                className='rounded-lg'
+                src='/icon.png'
+                alt='supplier'
+                width={240}
+                height={240}
+              />
+              <div className='ml-6'>
+                <div className='text-xl'>
+                  <span className='text-2xl font-semibold'>Invoy</span> - Trợ
+                  thủ đắc lực cho việc quản lý nhập hàng hoá của bạn
+                </div>
+                <div className='mt-6 text-md'>Với Invoy, bạn có thể</div>
+                <div className='space-y-2 mt-2'>
+                  <div className='text-base'>
+                    - Quản lý danh sách nhà cung cấp sản phẩm; quản lý sản phẩm
+                    trên từng nhà cung cấp
+                  </div>
+                  <div className='text-base'>
+                    - Quản lý việc nhập sản phẩm, quản lý công nợ cho từng nhà
+                    quản lý
+                  </div>
+                  <div className='text-base'>
+                    - Tạo hoá đơn thanh toán các sản phẩm đã nhập
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h1 className='text-xl font-bold text-center mb-4 mt-16'>
+            Tải xuống Invoy ngay hôm nay và bắt đầu quản lý hóa đơn như một
+            chuyên gia!
+          </h1>
+          <div className='flex justify-center'>
+            <Link href='https://apps.apple.com/app'>
+              <Image
+                className='object-cover'
+                src='/download-app-store.png'
+                alt='download on app store'
+                width={240}
+                height={72}
+              />
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className='py-16 bg-black'>
+        <div className='w-[720px] mx-auto'>
+          <Carousel autoplay>
+            <div className='h-[512px]'>
+              <div className='h-full flex justify-center'>
+                <img className='h-full' src='/carosel/1.png' alt='carosel' />
+              </div>
+            </div>
+            <div className='h-[512px]'>
+              <div className='h-full flex justify-center'>
+                <img className='h-full' src='/carosel/2.png' alt='carosel' />
+              </div>
+            </div>
+            <div className='h-[512px]'>
+              <div className='h-full flex justify-center'>
+                <img className='h-full' src='/carosel/3.png' alt='carosel' />
+              </div>
+            </div>
+            <div className='h-[512px]'>
+              <div className='h-full flex justify-center'>
+                <img className='h-full' src='/carosel/4.png' alt='carosel' />
+              </div>
+            </div>
+            <div className='h-[512px]'>
+              <div className='h-full flex justify-center'>
+                <img className='h-full' src='/carosel/5.png' alt='carosel' />
+              </div>
+            </div>
+          </Carousel>
+        </div>
+      </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className='w-[720px] mx-auto pt-16'>
+        <h2 className='text-3xl font-bold text-center mb-8'>
+          Nhập hàng hoá dễ dàng hơn bao giờ hết
+        </h2>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+          {benefits.map((benefit, index) => (
+            <div
+              key={index}
+              className='flex flex-col items-center p-4 bg-white rounded-lg shadow-md'
+            >
+              <h3 className='text-xl font-bold mb-2'>{benefit?.title}</h3>
+              <p className='text-gray-700'>{benefit?.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className='w-[720px] mx-auto guide mt-16'>
+        <h2 className='text-2xl font-bold mb-4'>Hướng dẫn sử dụng app</h2>
+        <Markdown>{guideContent}</Markdown>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className='w-[720px] mx-auto contact mt-16' id='contact-us'>
+        <h2 className='text-2xl font-bold mb-4'>Liên hệ</h2>
+        <Form form={form} layout='vertical' onFinish={handleSubmit}>
+          <Form.Item label='Họ tên' name='name' required>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label='Email'
+            name='email'
+            required
+            rules={[{ type: 'email', message: 'Nhập địa chỉ thư điện tử' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item label='Tin nhắn' name='message' required>
+            <Input.TextArea rows={6} />
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>
+              Gửi tin nhắn
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
+      <div className='h-24' />
     </main>
   );
 }
